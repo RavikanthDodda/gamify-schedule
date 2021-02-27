2bcbdfd71671788f2945ea8415c1b935d8863250
Ravikanth Reddy <ravikantreddy97@gmail.com>
2021-02-26 23:02:34 -0700

    Changed layout, button added

--- components/RegistrationPage.jsx
+++ components/RegistrationPage.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button, TextInput } from "react-native-paper";
+import { Button, TextInput, TextInputMask } from "react-native-paper";
 
 import { StyleSheet, Text, View } from "react-native";
 
 export default function RegistrationPage() {
-  const [text, setText] = React.useState("");
+  const [email, setEmail] = React.useState("");
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [phone, setPhone] = React.useState("");
@@ -14,38 +14,57 @@ export default function RegistrationPage() {
     <View style={styles.container}>
       <TextInput
         label="Email"
-        value={text}
-        style={styles.input}
-        onChangeText={(text) => setText(text)}
+        value={email}
+        style={styles.field}
+        keyboardType="email-address"
+        textContentType="emailAddress"
+        onChangeText={(email) => setEmail(email)}
       />
       <TextInput
         label="username"
         value={username}
+        style={styles.field}
+        textContentType="username"
         onChangeText={(username) => setUsername(username)}
       />
       <TextInput
         label="password"
         value={password}
+        style={styles.field}
+        textContentType="password"
+        secureTextEntry={true}
         onChangeText={(password) => setPassword(password)}
       />
       <TextInput
         label="phone"
         value={phone}
+        style={styles.field}
+        keyboardType="phone-pad"
+        textContentType="telephoneNumber"
         onChangeText={(phone) => setPhone(phone)}
       />
-      <Button mode="outlined">Register</Button>
+      <View style={styles.btn}>
+        <Button mode="contained">Register</Button>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: "flex",
+    padding: 30,
+    marginTop: 100,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
   },
-  input: {
-    marginBottom: 2,
+  field: {
+    marginBottom: 10,
+  },
+  btn: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
