b608d5f1a48b30ee3bcc34d90ba81627f70c3e86
shalikar <60168233+shalikar@users.noreply.github.com>
2021-03-16 17:04:00 -0700

    updated todo page

--- components/Pages/Todo.jsx
+++ components/Pages/Todo.jsx
@@ -5,13 +5,21 @@ import TodoItem from '../TodoItem'
 function Todo() {
   return (
     <View>
-      <Text style={{ flex: 1, alignItems: "center", justifyContent: "center", marginTop:100 }}>Todo page</Text>
+      <Text style={styles.header}>Todo List</Text>
       <TodoItem title='First to do item' description='Due: 03/17/2021'/>
       <TodoItem title='Second to do item' description='Due: 03/17/2021'/>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  header: {
+    textAlign: "center",
+    justifyContent: "center", 
+    marginTop: 40,
+    fontWeight: 'bold',
+    fontSize: 20,
+    },
+});
 
 export default Todo;
-
