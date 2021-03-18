624307dc9a4ccf1f0d5576e7a68341358e775fec
Ravikanth Reddy <ravikantreddy97@gmail.com>
2021-03-17 17:22:23 -0700

    Style changes to todo item

--- components/TodoItem.jsx
+++ components/TodoItem.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { List } from 'react-native-paper';
+import { Colors, List } from "react-native-paper";
 
 export default function TodoItem(props) {
-    return(
-        <View style={styles.container}>
-            <List.Item 
-                title={props.title}
-                description={props.description}
-                left={props => <List.Icon {...props} icon="select" />}
-                right={props => <List.Icon {...props} icon="delete" />}
-                style={styles.listItem}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <List.Item
+        title={props.title}
+        description={props.description}
+        left={(props) => <List.Icon {...props} icon="checkbox-blank-outline" />}
+        right={(props) => (
+          <List.Icon {...props} icon="delete" color={Colors.red400} />
+        )}
+        style={styles.listItem}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      marginTop: 10,
-      backgroundColor: "#fff",
-      justifyContent: "center",
-    },
-    listItem:{
-        backgroundColor: "lightblue",
-    },
-})
+  container: {
+    marginHorizontal: 10,
+    borderRadius: 8,
+    marginTop: 10,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  listItem: {
+    // backgroundColor: "lightblue",
+  },
+});
