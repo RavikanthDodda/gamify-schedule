ccf8e318df8453b2b0fb05bab2623aa074c86d0b
Ravikanth Reddy <ravikantreddy97@gmail.com>
2021-04-13 11:06:25 -0700

    task item changed, task can now be checked, notification upon checking added

--- Pages/ItemList.jsx
+++ Pages/ItemList.jsx
@@ -7,76 +7,76 @@ import Item from "../components/Item";
 import CustomerService from "../services/CustomerService";
 
 function ItemList(props) {
-  const { route, navigation } = props;
-  const [tasks, setTasks] = useState([]);
-  // const [showBar, setShowBar] = useState(false);
-  // let message = "";
-  // const notify = () => {
-  //   console.log(route.params);
-  //   if (route.params?.action) {
-  //     message = route.params.action;
-  //     setShowBar(true);
-  //   }
-  // };
+	const { route, navigation } = props;
+	const [tasks, setTasks] = useState([]);
+	const [showBar, setShowBar] = useState(false);
+	const [message, setMessage] = useState("");
 
-  const loadTasks = async () => {
-    let data;
-    switch (route.params?.item) {
-      case "schedule":
-        data = await CustomerService.getScheduleTasks();
-        break;
+	const notify = (mesg) => {
+		setMessage(mesg);
+		setShowBar(true);
+	}
 
-      case "todo":
-        data = await CustomerService.getTodoTasks();
-        break;
-      default:
-        break;
-    }
-    setTasks(data);
-  };
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      loadTasks();
-      // notify();
-    });
+	const loadTasks = async () => {
+		let data;
+		switch (route.params?.item) {
+			case "schedule":
+				data = await CustomerService.getScheduleTasks();
+				break;
 
-    return unsubscribe;
-  }, [navigation]);
+			case "todo":
+				data = await CustomerService.getTodoTasks();
+				break;
+			default:
+				break;
+		}
+		setTasks(data);
+	};
 
-  return (
-    <React.Fragment>
-      <FlatList
-        data={tasks}
-        renderItem={(item) => (
-          <Item
-            item={item}
-            navigation={navigation}
-            type={route.params.item}
-            page={route.params.formPage}
-          />
-        )}
-        keyExtractor={(task) => task.id}
-      />
-      <AddFAB
-        navigation={navigation}
-        page={route.params.formPage}
-        name={"Add new task"}
-      />
-      {/* <Snackbar
-        visible={showBar}
-        onDismiss={() => setShowBar(false)}
-        action={{
-          label: "Undo",
-          onPress: () => {
-            console.log("pressed");
-          },
-        }}
-      >
-        hi
-      </Snackbar> */}
-    </React.Fragment>
-  );
+	useEffect(() => {
+		const unsubscribe = navigation.addListener("focus", () => {
+			loadTasks();
+			// notify();
+		});
+
+		return unsubscribe;
+	}, [navigation]);
+
+	return (
+		<React.Fragment>
+			<FlatList
+				data={tasks}
+				renderItem={(item) => (
+					<Item
+						item={item}
+						navigation={navigation}
+						type={route.params.item}
+						page={route.params.formPage}
+						onComplete={notify}
+					/>
+				)}
+				keyExtractor={(task) => task.id}
+			/>
+			<AddFAB
+				navigation={navigation}
+				page={route.params.formPage}
+				name={"Add new task"}
+			/>
+			<Snackbar
+				visible={showBar}
+				onDismiss={() => setShowBar(false)}
+				action={{
+					label: "Ok",
+					onPress: () => {
+						console.log("pressed");
+					},
+				}}
+			>
+				{message}
+			</Snackbar>
+		</React.Fragment>
+	);
 }
 
 export default ItemList;
--- components/Item.jsx
+++ components/Item.jsx
@@ -1,38 +1,66 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import { List, Card } from "react-native-paper";
+import React, { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { IconButton } from "react-native-paper";
 
 export default function ListItem(props) {
-  const { item } = props.item;
+	const { item } = props.item;
+	const [ticked, setTicked] = useState(false);
 
-  return (
-    <Card style={styles.container}>
-      <List.Item
-        title={item.title}
-        description={item.description}
-        left={() => <List.Icon icon="checkbox-blank-outline" />}
-        // right={() => <List.Icon icon="delete" color={Colors.red400} />}
-        onPress={() => {
-          props.navigation.navigate(props.page, {
-            name: "Edit task",
-            taskId: item.id,
-          });
-        }}
-        style={styles.listItem}
-      />
-    </Card>
-  );
+	const getIcon = () => {
+		return ticked ? "checkbox-marked-circle" : "checkbox-blank-circle-outline";
+	}
+
+	const getStyle = () => {
+		const container = styles.container
+		container.backgroundColor = ticked ? "" : "#fff"
+		return container
+	}
+
+	return (
+		<View style={styles.container}>
+			<IconButton icon={getIcon()} color={ticked ? "#858585" : "#000"} onPress={
+				() => {
+					setTicked(!ticked);
+					if (!ticked)
+						props.onComplete("Task finished");
+				}
+			} />
+			<View style={styles.listItem}>
+				<TouchableOpacity style={{ height: "100%", width: "100%", justifyContent: "center" }}
+					onPress={() => {
+						props.navigation.navigate(props.page, {
+							name: "Edit task",
+							taskId: item.id,
+						});
+					}}>
+					<Text style={{ fontWeight: "700", color: ticked ? "#858585" : "#000" }}> {item.title}</Text>
+					{/* TODO - add repeat info */}
+					<View>
+						<Text style={{ color: "#858585" }}> {item.description}</Text>
+
+						{/* <Text style={{ color: "#858585" }}> {item.}</Text> */}
+					</View>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 20,
-    borderRadius: 8,
-    marginTop: 10,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-  },
-  listItem: {
-    // backgroundColor: "lightblue",
-  },
+	container: {
+		height: 70,
+		marginHorizontal: 20,
+		borderRadius: 8,
+		marginTop: 10,
+		backgroundColor: "#fff",
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	icon: {
+	},
+	listItem: {
+		flex: 2,
+		height: "100%",
+	},
 });
